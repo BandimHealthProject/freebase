1eff1f489092d7db28111b59294e2a8deda314a3
AndrMJ <48027984+AndrMJ@users.noreply.github.com>
2019-11-27 18:22:54 +0000

    Update freebaseHelpers.js

--- app/system/survey/js/freebaseHelpers.js
+++ app/system/survey/js/freebaseHelpers.js
@@ -4,6 +4,24 @@ return {
     echo: function(str) {
         console.log("****** ECHO CALLED ******");
         alert(str);
+    },
+    decimalPlaces: function(num) {
+        //https://stackoverflow.com/questions/10454518/javascript-how-to-retrieve-the-number-of-decimals-of-a-string-number
+        var match = (''+num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+        if (!match) { return 0; }
+        return Math.max(
+            0,
+            // Number of digits right of decimal point.
+            (match[1] ? match[1].length : 0)
+            // Adjust for scientific notation.
+            - (match[2] ? +match[2] : 0));
+    },
+    studyNumber: function(letters,zeros,num) {
+        var number = num + ""
+        while (number.length < zeros) {
+            number = "0" + number
+        };
+        return letters + number;
     }
 }
 });
